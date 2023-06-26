c02f7a8b3cb37caac0dc8a01f802bf8633091817
Atreyye12 <102455332+Atreyye12@users.noreply.github.com>
2023-06-26 16:02:43 -0700

    Add files via upload

---
+++ swappingFile.py
@@ -0,0 +1,15 @@
+def swapFileData():
+    file1 = ('sample1.txt')
+    file2 = ('sample2.txt')
+
+    with open(file1, 'r') as a:
+        data_a = a.read()
+    with open(file2, 'r') as a:
+        data_b = a.read()
+    with open(file1, 'w') as a:
+        a.write(data_b)
+    with open(file2, 'w') as a:
+        a.write(data_a)
+
+swapFileData()
+    
